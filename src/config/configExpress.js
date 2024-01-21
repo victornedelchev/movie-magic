c12e4bffd65d0505bba772216fd5799f06de5eaf
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-01-21 11:28:12 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 function configExpress(app) {
   app.use(express.static(path.resolve('src/public')));
+  app.use(express.urlencoded({ extended: false }));
 
   return app;
 }
