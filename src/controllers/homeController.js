3743a82c019e888f144043c88063107c24ba7b5d
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-01-21 18:42:35 +0200

    Show dynamic movies on search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,6 +12,13 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+    res.render('search', {
+        movies
+    });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
