e0fcca7593b91bf6a59b38009cc9bc482bed72b3
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-01-27 10:56:38 +0200

    Refactor movieservice to use Movie model

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,30 +1,16 @@
 const Movie = require('../models/Movie');
 
-const movies = [{
-    _id: 1,
-    title: 'Jungle Cuise',
-    genre: 'Adventure',
-    director: 'Jaume Collet-Serra',
-    year: '2021',
-    imageUrl: '/img/jungle-cruise.jpeg',
-    rating: '5',
-    description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'
-}];
-
-exports.getAll = () => {
-    return movies.slice();
-    // return [...movies];
-    // return Array.from(movies);
-};
+exports.getAll = () => Movie.find();
 
 exports.getOne = (movieId) => {
-   const movie = movies.find(movie => movie._id == movieId);
+   const movie = Movie.findById(movieId);
    
    return movie;
 }; 
 
-exports.search = (title, genre, year) => {
-    let result = movies.slice();
+// TODO: fIlter result in mongoDB
+exports.search = async (title, genre, year) => {
+    let result = await Movie.find().lean();
 
     if (title) {
         result = result.filter(movie => movie.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()));
@@ -41,8 +27,6 @@ exports.search = (title, genre, year) => {
     return result;
 };
 
-exports.create = async (movieData) => {
-   const result = await Movie.create(movieData);
+exports.create = (movieData) => Movie.create(movieData);
 
-   return result;
-};
+  
