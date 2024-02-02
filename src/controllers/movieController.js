98c95c16ddc00afd1a692d32b570dc5d06d154c1
Victor Nedelchev <victornedelchev@yahoo.com>
2024-02-02 21:30:38 +0200

    Add configuration rendering of details buttons

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -26,12 +26,16 @@ router.get("/movies/:movieId", async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId).lean();
   // const casts = await castService.getByIds(movie.casts).lean();
+  const isOwner = movie.owner == req.user._id;
+  // const isOwner = movie.owner.toString() === req.user._id;
+  // const isOwner = movie.owner === mongoose.Types.ObjectId(req.user._id);
 
   // TODO: This is not perfect, use handlebars helpers
   movie.rating = new Array(Number(movie.rating)).fill(true);
 
-  res.render("/movie/details", {
+  res.render("movie/details", {
     movie,
+    isOwner,
   });
 });
 
