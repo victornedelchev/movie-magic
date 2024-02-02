3be8db8c160380d31afc5faaed3b7bfab02ec12d
Victor Nedelchev <victornedelchev@yahoo.com>
2024-02-02 20:44:01 +0200

    Move details view to movie folder

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -27,7 +27,7 @@ router.get('/movies/:movieId', async (req, res) => {
     // TODO: This is not perfect, use handlebars helpers
     movie.rating = new Array(Number(movie.rating)).fill(true);
 
-    res.render('details', {
+    res.render('/movie/details', {
         movie
     });
 });
