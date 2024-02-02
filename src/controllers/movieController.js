cbbfb675d01365cd062366e168726126385775ce
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-02-02 17:04:51 +0200

    Add edit page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -51,4 +51,8 @@ router.post('/movies/:movieId/attach', async (req, res) => {
     res.redirect(`/movies/${movieId}/attach`);
 });
 
+router.get('/movies/:movieId/edit', (req, res) => {
+    res.render('movie/edit')
+})
+
 module.exports = router;
