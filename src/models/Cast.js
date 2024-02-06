f2d2db8a80d1a965af7810d0c957d2e3c2679cce
Victor Nedelchev <victornedelchev@yahoo.com>
2024-02-06 21:39:03 +0200

    Add additional validation to castSchema

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -3,21 +3,26 @@ const mongoose = require('mongoose');
 const castSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name mist be at least 5 characters long!'],
     },
     age: {
         type: Number,
-        required: true,
-        min: 5,
-        max: 120
+        required:[true, 'Age is required!'],
+        min: [1, 'Age must be between 1 - 120!'],
+        max: [120, 'Age must be between 1 - 120!'],
     },
     born: {
         type: String,
-        required: true
+        required: [true, 'Born is required!'],
+        minLength: [10, 'Born mist be at least 10 characters long!'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Born could be English letters, digits, and white spaces!'],
     },
     nameInMovie: {
         type: String,
-        required: true
+        required: [true, 'Name in movie is required!'],
+        minLength: [5, 'Name in movie mist be at least 5 characters long!'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces!'],
     },
     castImage: {
         type: String,
