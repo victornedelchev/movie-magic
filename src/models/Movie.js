0dfc7a6dcc8ada18da77e9e5a1f5caea832655b2
Victor Nedelchev <victornedelchev@yahoo.com>
2024-02-06 21:21:59 +0200

    Fix movieSchema

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -5,20 +5,20 @@ const movieSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Title is required!'],
         minLength: [5, 'Title mist be at least 5 characters long!'],
-        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces!'],
     },
     genre: {
         type: String,
         required: [true, 'Genre is required!'],
         lowercase: true,
         minLength: [5, 'Genre mist be at least 5 characters long!'],
-        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces!'],
     },
     director:  {
         type: String,
         required: [true, 'Director is required!'],
         minLength: [5, 'Director mist be at least 5 characters long!'],
-        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Director could be English letters, digits, and white spaces!'],
     },
     year: {
         type: Number,
@@ -36,12 +36,12 @@ const movieSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Description is required!'],
         manLength: 20,
-        match: [/^[A-Za-z0-9\s]+$/g, 'Title could be English letters, digits, and white spaces'],
+        match: [/^[A-Za-z0-9\s]+$/g, 'Description could be English letters, digits, and white spaces!'],
     },
     imageUrl: {
         type: String,
         required: [true, 'Image URL is required!'],
-        match: /^https?:\/\//,
+        match: [/^https?:\/\//, 'Image URL starts with http://... or https://...!'],
     },
     casts: [{
         type: mongoose.Types.ObjectId,
